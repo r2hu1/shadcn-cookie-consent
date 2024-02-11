d3f380dcbe61fd10eb9df9c8f8fe5b0243683ce4
r2hu1 <newasram86@gmail.com>
2024-02-11 23:27:08 +0530

    favicon

--- app/layout.js
+++ app/layout.js
@@ -9,8 +9,20 @@ const poppins = Poppins({
 
 export const metadata = {
   title: "Shadcn Cookie COnsent",
-  description: "Beautifully designed, customizable cookie consent for web built on top of shadcn-ui!",
+  description: "Beautifully designed, customizable cookie consent for web built on top of shadcn-ui and tailwind-css!",
+  keywords: "shadcn, cookie consent, nextjs, tailwind, ui, components",
+  author: "r2hu1",
+  robots: "index, follow",
+  icons:["/logo.png"]
 };
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  themeColor: "#09090b",
+}
 
 export default function RootLayout({ children }) {
   return (
