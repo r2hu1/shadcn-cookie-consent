117ed60b9ac74e31b687339abd9abbffdead4cd4
r2hu1 <newasram86@gmail.com>
2024-02-11 23:36:21 +0530

    margin

--- components/CookieConsent.jsx
+++ components/CookieConsent.jsx
@@ -45,7 +45,7 @@ export default function CookieConsent({ demo = false, onAcceptCallback = () => {
 
     return (
         <div className={cn("fixed z-[200] bottom-0 left-0 right-0 sm:left-4 sm:bottom-4 w-full sm:max-w-md transition-transform duration-700", !isOpen ? "transition-[opacity,transform] translate-y-8 opacity-0" : "transition-[opacity,transform] translate-y-0 opacity-100", hide && "hidden")}>
-            <div className="bg-secondary rounded-md m-4">
+            <div className="bg-secondary rounded-md m-2">
                 <div className="grid gap-2">
                     <div className="border-b border-border h-14 flex items-center justify-between p-4">
                         <h1 className="text-lg font-medium">We use cookies</h1>
