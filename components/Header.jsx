cfb1b971d5132109c3408403041561d2cdca57dc
r2hu1 <newasram86@gmail.com>
2024-02-11 23:27:27 +0530

    header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,4 @@
+import { CookieIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { FaCodeFork, FaGithub } from "react-icons/fa6";
 
@@ -5,11 +6,11 @@ export default function Header() {
     return (
         <header className="py-5 px-6 md:px-20 lg:px-32 flex items-center justify-between">
             <div>
-
+                <CookieIcon className="h-[1.2rem] w-[1.2rem]" />
             </div>
             <div className="flex items-center gap-2">
-                <Button size="icon" asChild><a href="https://github.com/r2hu1/shadcn-cookie-consent"><FaGithub className="h-[1.1rem] w-[1.1rem]"/></a></Button>
-                <Button size="icon" variant="secondary" asChild><a href="https://github.com/r2hu1/shadcn-cookie-consent/fork"><FaCodeFork className="h-[1.1rem] w-[1.1rem]"/></a></Button>
+                <Button size="icon" asChild><a href="https://github.com/r2hu1/shadcn-cookie-consent"><FaGithub className="h-[1.1rem] w-[1.1rem]" /></a></Button>
+                <Button size="icon" variant="secondary" asChild><a href="https://github.com/r2hu1/shadcn-cookie-consent/fork"><FaCodeFork className="h-[1.1rem] w-[1.1rem]" /></a></Button>
             </div>
         </header>
     )
