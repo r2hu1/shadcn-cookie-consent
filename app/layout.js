e11aeba3e32ab5a56a52c91590e880ad78bdb6ad
r2hu1 <newasram86@gmail.com>
2024-02-11 23:57:45 +0530

    footer

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
       <body className={poppins.className}>
         <Header/>
         {children}
+        <Footer/>
         </body>
     </html>
   );
---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="py-10 text-center px-6">
+            <div>
+                <p className="text-sm font-normal">Built by <a href="https://github.com/r2hu1" className="underline">r2hu1</a>. source code avialable on <a className="underline" href="https://github.com/r2hu1/shadcn-cookie-consent">github</a>.</p>
+            </div>
+        </footer>
+    )
+}
