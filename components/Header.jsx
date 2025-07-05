3d3223cbb4671d06ba3c5f48b027e4372f6bbbcd
r2hu1 <newasram86@gmail.com>
2025-07-05 13:25:50 +0530

    feat: added installation btn

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,7 @@ import { CookieIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { FaCodeFork, FaGithub } from "react-icons/fa6";
 import { ModeToggle } from "./ModeToggle";
+import { Inspect } from "lucide-react";
 
 export default function Header() {
   return (
@@ -11,6 +12,15 @@ export default function Header() {
       </div>
       <div className="flex items-center gap-2">
         <Button size="sm" asChild className="rounded-xl">
+          <a
+            className="flex items-center gap-2"
+            href="https://github.com/r2hu1/shadcn-cookie-consent/blob/master/README.md"
+          >
+            <Inspect className="h-4 w-4" />
+            Installation
+          </a>
+        </Button>
+        <Button size="sm" variant="secondary" asChild className="rounded-xl">
           <a
             className="flex items-center gap-2"
             href="https://github.com/r2hu1/shadcn-cookie-consent"
